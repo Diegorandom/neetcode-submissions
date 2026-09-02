2dd6788b6d24488e14d5e9235f42c86bc8c844ab
Diego Ignacio Ortega Martínez <diegortegaimi@gmail.com>
2026-09-02 17:39:34 -0600

    Add: longest-consecutive-sequence - submission-5

---
+++ Data Structures & Algorithms/longest-consecutive-sequence/submission-5.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    int longestConsecutive(vector<int>& nums) {
+        sort(nums.begin(), nums.end());
+        copy(nums.begin(), nums.end(), ostream_iterator<int>(cout, " "));
+        int localMax = 0, prefix = 0, tMax=0;
+        for(int i=0; i<nums.size(); i++){
+            if(i==0){
+                prefix = nums[i];
+                localMax = 1;
+                tMax = max(localMax, tMax);
+            }
+            if((nums[i] - prefix) == 1){
+                localMax += 1;
+                prefix = nums[i];
+                tMax = max(localMax, tMax);
+            }if((nums[i] - prefix) != 0) {
+                prefix = nums[i];
+                tMax = max(localMax, tMax);
+                localMax=1;
+            }
+        }
+        return tMax;
+    }
+};
