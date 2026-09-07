e17a289e523100a4322df397fd61445c0e42c06d
Diego Ignacio Ortega Martínez <diegortegaimi@gmail.com>
2026-09-06 18:44:11 -0600

    Add: two-integer-sum-ii - submission-10

---
+++ Data Structures & Algorithms/two-integer-sum-ii/submission-10.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& numbers, int target) {
+        int left=0,right=0,idx=0,idx2=numbers.size()-1;
+        vector<int> output;
+        while(true){
+            left=numbers[idx];
+            right=numbers[idx2];
+            if((left+right) < target){
+                idx++;
+            }else if((left+right) > target){
+                idx2--;
+            }else{
+                break;
+            }
+        }
+        output.push_back(idx+1);
+        output.push_back(idx2+1);
+        return output;
+    }
+};
