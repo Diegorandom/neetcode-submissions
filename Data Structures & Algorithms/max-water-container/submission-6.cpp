9f4e78133e0766da90eb4512ceb68c987b7d68f6
Diego Ignacio Ortega Martínez <diegortegaimi@gmail.com>
2026-09-07 12:42:02 -0600

    Add: max-water-container - submission-6

---
+++ Data Structures & Algorithms/max-water-container/submission-6.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    int maxArea(vector<int>& heights) {
+        int left=0, right=heights.size()-1, res=0;
+        auto max_it = max_element(heights.begin(), heights.end());
+        int max_value = *max_it;
+        int i=heights[left],j=heights[right];
+        while(left<right){
+            int a = (right-left)*min(i, j);
+            res = max(res,a);
+            if(i<max_value && i<j){
+                left++;
+            } else if(j<max_value){
+                right--;
+            } else {
+                break;
+            }
+            i=heights[left];
+            j=heights[right];
+        }
+        return res;
+    }
+};
