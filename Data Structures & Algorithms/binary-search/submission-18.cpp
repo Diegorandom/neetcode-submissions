5ded372b26603c6fc5c9174cac65ad3c399a5640
Diego Ignacio Ortega Martínez <diegortegaimi@gmail.com>
2026-09-12 17:06:30 -0600

    Add: binary-search - submission-18

---
+++ Data Structures & Algorithms/binary-search/submission-18.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    int search(vector<int>& nums, int target) {
+        int lp=0, rp=nums.size()-1;
+        int mid = (rp-lp)/2;
+        while(rp>=lp){
+            cout << lp;  
+            cout << rp;
+            cout << mid;
+            if(target<nums[mid]){
+                rp=mid-1;
+                mid = lp+((rp-lp)/2);
+            } else if(target>nums[mid]){
+                lp=mid+1;
+                mid = lp+ ((rp-lp)/2);
+            } else if(target == nums[mid]){
+                return mid;
+            }
+        }
+        return -1;
+    }
+};
